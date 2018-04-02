a62f83d7c5fc400eee9cfb4a1ee80ccf917c5d1a
Noah Day <sheltielover432@gmail.com>
2018-04-02 19:12:19 -0400

    Commit 1
    Commit of beginnings of step 1

--- hello-cloud/src/app/app.module.ts
+++ hello-cloud/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeroesComponent
   ],
   imports: [
     ReactiveFormsModule,
